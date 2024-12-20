dad18eebfdda234cb056927b32795a91947bf53d
Andre Izarra <programador385@gmail.com>
2024-12-20 18:28:32 -0400

    Admin Navigation sidebar links and icons

--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -1,12 +1,31 @@
 import { Link, Outlet } from "@remix-run/react";
 import {
+  ChevronDown,
+  Users,
+  Users2,
+  Settings,
+  Home,
+  HandCoins,
+  CalendarArrowDown,
+  Tag,
+} from "lucide-react";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "~/components/ui/collapsible";
+import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
-  SidebarHeader,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
   SidebarMenu,
+  SidebarMenuButton,
   SidebarMenuItem,
   SidebarProvider,
+  SidebarTrigger,
 } from "~/components/ui/sidebar";
 
 export default function AdminLayout() {
@@ -15,6 +34,7 @@ export default function AdminLayout() {
       <SidebarProvider>
         <AdminSidebar />
         <main className="w-full p-4 bg-gray-100">
+          <SidebarTrigger />
           <Outlet />
         </main>
       </SidebarProvider>
@@ -22,28 +42,96 @@ export default function AdminLayout() {
   );
 }
 
-const links = [
-  { href: "/admin/users", text: "Usuarios" },
-  { href: "/admin/clients", text: "Clientes" },
+const outerLinks = [
+  { href: "/admin/products/list", text: "Productos", icon: Tag },
+  { href: "/admin/orders/list", text: "Ordenes", icon: CalendarArrowDown },
+  { href: "/admin/users/list", text: "Usuarios", icon: Users },
+  { href: "/admin/clients/list", text: "Clientes", icon: Users2 },
+];
+
+const configLinks = [
+  { href: "/admin/categories/list", text: "Categorías" },
+  { href: "/admin/brands/list", text: "Marcas" },
+  { href: "/admin/currencies/list", text: "Monedas" },
+  { href: "/admin/locations/list", text: "Sucursales" },
+  { href: "/admin/suppliers/list", text: "Proveedores" },
+  { href: "/admin/promotions/list", text: "Promociones" },
+  { href: "/admin/shipping/list", text: "Envíos" },
+];
+
+const adminLinks = [
+  { href: "/admin/delivery-notes/list", text: "Notas de entrega" },
+  { href: "/admin/invoices/list", text: "Facturas" },
 ];
 
 function AdminSidebar() {
   return (
     <Sidebar className="p-4">
-      <SidebarHeader className="text-2xl bold mb-4">Admin</SidebarHeader>
       <SidebarContent>
-        <SidebarMenu className="flex flex-col gap-2">
-          {links.map((link) => (
-            <SidebarMenuItem key={link.href}>
-              <Link
-                className="group relative my-px flex min-h-[2.25rem] items-center rounded-2xl border-transparent px-3 py-2 outline-none transition-colors duration-100 focus-visible:ring-2 focus-visible:ring-inset focus-visible:ring-blue-800 dark:focus-visible:ring-gray-100 text-gray-700 hover:text-black dark:text-gray-300 dark:hover:text-gray-100 hover:bg-blue-100 dark:hover:bg-blue-800/50"
-                to={link.href}
-              >
-                {link.text}
-              </Link>
-            </SidebarMenuItem>
-          ))}
-        </SidebarMenu>
+        <SidebarGroup>
+          <SidebarGroupLabel>
+            <Home className="mr-2" />
+            Principal
+          </SidebarGroupLabel>
+          <SidebarMenu>
+            {outerLinks.map((link) => (
+              <SidebarMenuItem key={link.href}>
+                <SidebarMenuButton asChild>
+                  <Link to={link.href}>
+                    <link.icon />
+                    {link.text}
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
+        <SidebarGroup>
+          <Collapsible className="group/collapsible">
+            <SidebarGroupLabel asChild>
+              <CollapsibleTrigger>
+                <HandCoins className="mr-2" /> Administración
+                <ChevronDown className="ml-4 transition-transform group-data-[state=open]/collapsible:rotate-180" />
+              </CollapsibleTrigger>
+            </SidebarGroupLabel>
+            <CollapsibleContent>
+              <SidebarGroupContent>
+                <SidebarMenu className="flex flex-col gap-2">
+                  {adminLinks.map((link) => (
+                    <SidebarMenuItem key={link.href}>
+                      <SidebarMenuButton asChild>
+                        <Link to={link.href}>{link.text}</Link>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  ))}
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </CollapsibleContent>
+          </Collapsible>
+        </SidebarGroup>
+        <SidebarGroup>
+          <Collapsible className="group/collapsible">
+            <SidebarGroupLabel asChild>
+              <CollapsibleTrigger>
+                <Settings className="mr-2" /> Configuración
+                <ChevronDown className="ml-4 transition-transform group-data-[state=open]/collapsible:rotate-180" />
+              </CollapsibleTrigger>
+            </SidebarGroupLabel>
+            <CollapsibleContent>
+              <SidebarGroupContent>
+                <SidebarMenu className="flex flex-col gap-2">
+                  {configLinks.map((link) => (
+                    <SidebarMenuItem key={link.href}>
+                      <SidebarMenuButton asChild>
+                        <Link to={link.href}>{link.text}</Link>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  ))}
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </CollapsibleContent>
+          </Collapsible>
+        </SidebarGroup>
       </SidebarContent>
       <SidebarFooter />
     </Sidebar>
