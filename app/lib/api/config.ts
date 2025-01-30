c38ff5705b779dcc359f05b57b102c235cb953f2
Andre Izarra <programador385@gmail.com>
2025-01-30 14:39:50 -0400

    config url

--- app/lib/api/config.ts
+++ app/lib/api/config.ts
@@ -1,6 +1,6 @@
-export const API_URL = `http://localhost:5555/graphql`;
-//export const API_URL =
-//  "https://on-the-court-58db5a5ee750.herokuapp.com/graphql";
+//export const API_URL = `http://localhost:5555/graphql`;
+export const API_URL =
+  "https://on-the-court-58db5a5ee750.herokuapp.com/graphql";
 
 export const PAGINATION_CONFIG = {
   limit: 10,
